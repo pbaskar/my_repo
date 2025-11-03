221edb85e0dda56f36e17daf9bf540220a4de394
Priya <pbaskar>
2025-11-03 12:03:49 -0500

    Fix includes CodeAnalyzer does not expect commandline arguments

--- sca/analyzer/CodeAnalyzer/main.cpp
+++ sca/analyzer/CodeAnalyzer/main.cpp
@@ -6,12 +6,12 @@ char* gFileName;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    if(argc < 2) {
+    /*if (argc < 2) {
         std::cout <<"Not enough arguments " <<std::endl;
         return 0;
     }
     gFileName = argv[1];
-    std::cout << "command line argument :: file name " <<gFileName <<std::endl;
+    std::cout << "command line argument :: file name " <<gFileName <<std::endl;*/
     SocketServer socketServer;
     return a.exec();
 }
